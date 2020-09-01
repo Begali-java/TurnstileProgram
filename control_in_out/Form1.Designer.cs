d056667c88ae84074de32634d109726f17ae3190
Begali-java <62948448+Begali-java@users.noreply.github.com>
2020-09-01 13:47:07 +0500

    demo

---
+++ control_in_out/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace control_in_out
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tb_device_ip = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.p_main = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_cardno = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.p_main.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tb_device_ip
+            // 
+            this.tb_device_ip.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_device_ip.Location = new System.Drawing.Point(104, 22);
+            this.tb_device_ip.Name = "tb_device_ip";
+            this.tb_device_ip.Size = new System.Drawing.Size(221, 30);
+            this.tb_device_ip.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Device IP :";
+            // 
+            // p_main
+            // 
+            this.p_main.Controls.Add(this.label3);
+            this.p_main.Controls.Add(this.tb_cardno);
+            this.p_main.Controls.Add(this.label2);
+            this.p_main.Controls.Add(this.tb_id);
+            this.p_main.Controls.Add(this.button2);
+            this.p_main.Enabled = false;
+            this.p_main.Location = new System.Drawing.Point(12, 75);
+            this.p_main.Name = "p_main";
+            this.p_main.Size = new System.Drawing.Size(1087, 572);
+            this.p_main.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(17, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Card no:";
+            // 
+            // tb_cardno
+            // 
+            this.tb_cardno.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_cardno.Location = new System.Drawing.Point(120, 70);
+            this.tb_cardno.Name = "tb_cardno";
+            this.tb_cardno.Size = new System.Drawing.Size(236, 27);
+            this.tb_cardno.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(17, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Id :";
+            // 
+            // tb_id
+            // 
+            this.tb_id.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_id.Location = new System.Drawing.Point(120, 21);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(105, 27);
+            this.tb_id.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(409, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 30);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Registration";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1111, 648);
+            this.Controls.Add(this.p_main);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_device_ip);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.p_main.ResumeLayout(false);
+            this.p_main.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tb_device_ip;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel p_main;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_id;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_cardno;
+    }
+}
+
---
+++ control_in_out/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+
+namespace control_in_out
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("plcommpro.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern int Connect(string s);
+
+        // To search devices
+        [DllImport("plcommpro.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int SearchDevice(string p1, string p2, ref byte p3);
+
+
+        // To add users
+        //  handle
+        //      [in]: The handle that is returned when the connection is successful.
+        //  TableName
+        //      [in]: Data table name. Attached table 4 lists the available data tables.
+        //  Data
+        //      [in]: Data record; the data is expressed in a text format; the multiple records are separated by \r\n, and the “Field=Value” pairs are separated by \t.
+        //  Options
+        //      [in]: The default value is null; it is used for extension.
+        //  [Returned value]
+        //      When the returned value is 0, it indicates that the operation is successful. When the returned value is a negative value, 
+        //      it indicates that the operation fails. Attached table 5 lists the information about the error codes.
+
+        [DllImport("plcommpro.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int SetDeviceData(int handle, string TableName, string Data, string Options);
+        
+        static int result;
+
+        public Form1()
+        {
+            InitializeComponent();
+            result = -1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           result = Connect("protocol=TCP,ipaddress=" + tb_device_ip.Text + ",port=4370,timeout=2000,passwd=");
+           if (result > 0)
+           {
+               SetDeviceData(result, "timezone",
+               "TimezoneId=1\tSunTime1=2359\tMonTime1=2359\tTueTime1=2359\tWedTime1=2359\tThuTime1=2359\tFriTime1=2359\tSatTime1=2359", "");
+               p_main.Enabled = true;
+           }
+           else MessageBox.Show("Connect to device fail!");
+           
+        }
+
+        // To add card
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int ret = -1;
+            string data = "Pin=" + tb_id.Text + "\tCardNo=" + tb_device_ip.Text + "\tPassword=1";
+
+            ret = SetDeviceData(result,"user",data,"");
+            if (ret == 0)
+                MessageBox.Show("Add card to user Success!");
+            else
+                MessageBox.Show("Add card to user Fail!");
+            data = "Pin=" + tb_id.Text + "\tAuthorizeTimezoneId=1\tAuthorizeDoorId=3";
+
+            
+            ret = SetDeviceData(result, "userauthorize", data, "");
+            if (ret == 0)
+                MessageBox.Show("Add user to userauthorize Success!");
+            else
+                MessageBox.Show("Add user to userauthorize Fail!");
+
+            
+        }
+
+        
+    }
+}
